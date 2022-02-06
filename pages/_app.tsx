d090cea2a316b09952ade3543f72e2d575b7cbee
Merritt Blanks <mhblanks@gmail.com>
2022-02-06 10:55:25 -0700

    fonts from cdn

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,4 @@
 import '../styles/globals.css'
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 
 import * as React from 'react';
 import Head from 'next/head';
