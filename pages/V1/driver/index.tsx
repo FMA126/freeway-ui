42ca3407f1f460b8eae5adefd9648de505647c15
Merritt Blanks <mhblanks@gmail.com>
2022-02-07 17:40:50 -0700

    remove eslint 3rd party plugins

--- pages/V1/driver/index.tsx
+++ pages/V1/driver/index.tsx
@@ -1,7 +1,28 @@
+import { useWeb3React } from '@web3-react/core'
+
 export default function Driver() {
+  // { active, account, activate, connector, deactivate, error, library }
+  const { active, account, library } = useWeb3React()
+  console.log(account)
+
+  const handleDriverMint = async () => {
+    const signer = library.getSigner(account)
+    // const driverContract = FreeWayDriver__factory.connect(DRIVER_MUMBAI_ADDRESS, signer)
+    // await driverContract.mintItem(account || '')
+  }
+  if (!active) {
+    return (
+      <>
+        <div>Please download MetaMask</div>
+      </>
+    )
+  }
   return (
-    <>
-      <div>Driver Page</div>
-    </>
+    <div className="mint-container-driver">
+      <h1>Mint Driver</h1>
+      <button onClick={handleDriverMint}>
+        Mint <span>🏎</span>
+      </button>
+    </div>
   )
 }
--- pages/V1/rider/index.tsx
+++ pages/V1/rider/index.tsx
@@ -1,7 +1,27 @@
-export default function Rider() {
+import { useWeb3React } from '@web3-react/core'
+
+export default function MintRider() {
+  // { active, account, activate, connector, deactivate, error, library }
+  const { active, account, library } = useWeb3React()
+
+  const handleRiderMint = async () => {
+    const signer = library.getSigner(account)
+    // const riderContract = FreeWayRider__factory.connect(RIDER_MUMBAI_ADDRESS, signer)
+    // await riderContract.mintItem(account || '')
+  }
+  if (!active) {
+    return (
+      <>
+        <div>Please download MetaMask</div>
+      </>
+    )
+  }
   return (
-    <>
-      <div>Rider Page</div>
-    </>
+    <div className="mint-container-rider">
+      <h1>Mint Rider</h1>
+      <button onClick={handleRiderMint}>
+        Mint <span>🎢</span>
+      </button>
+    </div>
   )
 }
